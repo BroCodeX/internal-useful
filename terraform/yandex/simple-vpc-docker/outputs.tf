40bd3964417b31a61030b3df8bd88218c90508d1
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2025-01-24 07:38:48 +0300

    Update outputs.tf

--- terraform/yandex/simple-vpc-docker/outputs.tf
+++ terraform/yandex/simple-vpc-docker/outputs.tf
@@ -1,1 +1,14 @@
+output "external_ip" {
+  description = "An external ip of the instance"
+  value = yandex_compute_instance.this.network_interface[0].nat_ip_address
+}
 
+output "boot_disk_id" {
+  description = "The ID of the boot disk created for the instance."
+  value       = yandex_compute_disk.boot_disk.id
+}
+
+output "instance_id" {
+  description = "The ID of the Yandex Compute instance."
+  value       = yandex_compute_instance.this.id
+}
